9715c78f1c73fc1408e7ecbb47c4bcc04945e8b4
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2022-12-01 10:27:00 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-    print(f"{ch:c}", end='') #printing alphabet using the ascii table
+for ch in range(97, 123): #printing alphabet in small case on the ascii table
+   print("{:c}" .format(ch), end='')
