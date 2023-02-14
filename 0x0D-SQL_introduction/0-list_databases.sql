1c01223e5ebc098e53e163861b4a08f27f6d30c5
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-02-14 13:07:13 -0800

    Databases tasks

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,5 @@
 -- This task is all about listing out all databases in MYSQL.
 -- Code Displaying all databases in mysql
 
-mysql -u root -p  -e 'SHOW DATABASES'
+mysql -u root -p -e 'SHOW DATABASES;'
 
