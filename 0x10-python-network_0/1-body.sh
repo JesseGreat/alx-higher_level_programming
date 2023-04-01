223fba78514fe5e2e796e20534bd3bd57eb902e6
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-01 16:55:28 -0700

    curl delete

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+# This gets the response body for a given URL for 200 status code responses.
 curl -sL "$1"
