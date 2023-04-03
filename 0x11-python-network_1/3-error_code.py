700f1568ae6a0dd98c0354f70a1e85bcf3a2dd88
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 09:44:32 -0700

    error handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,17 +9,10 @@ import sys
 import urllib.request
 import urllib.error
 
-if __name__ == '__main__':
-    # Get the URL from the command line arguments
-    url = sys.argv[1]
-
+if __name__ == "__main__":
     try:
-        # Send a request to the URL and get the response
-        with urllib.request.urlopen(url) as response:
-            # Decode the response body and print it
+        with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
-
     except urllib.error.HTTPError as e:
-        # If an HTTP error occurs, print the HTTP status code
         print(f"Error code: {e.code}")
 
