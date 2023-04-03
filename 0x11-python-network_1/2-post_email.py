b757f816b52666ca8f45421725490e0b27200c98
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 09:23:26 -0700

    adjustment on post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 
-if __name__ == '__main__':
-    # Get URL and email from command line arguments
+"""This script sends a POST request to the passed URL
+    with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    # Get the URL and email from command line arguments
     url = sys.argv[1]
     email = sys.argv[2]
 
-    # Encode the email parameter
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    # Encode the email as a URL parameter
+    params = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-    # Send the POST request and read the response
-    with urllib.request.urlopen(url, data=data) as response:
-        html = response.read()
+    # Send a POST request to the URL with the email as a parameter
+    with urllib.request.urlopen(url, data=params) as response:
+        # Read and decode the response body in utf-8
+        body = response.read().decode('utf-8')
 
-    # Decode the response and print it
-    print(html.decode('utf-8'))
+    # Print the response body
+    print(body)
 
