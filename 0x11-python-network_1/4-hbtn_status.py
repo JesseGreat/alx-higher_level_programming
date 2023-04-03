6b9c1b59732c9542a49d7fa5e814dea4dba1edcd
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 09:51:37 -0700

    using request module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This script fetches the status page at https://alx-intranet.hbtn.io/status using
+the requests package and displays the response body.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    body = response.text
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+
