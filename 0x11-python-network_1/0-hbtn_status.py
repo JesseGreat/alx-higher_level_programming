1fac11a9e75f509776317d94a8f514b101adf086
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 04:26:58 -0700

    adjustments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,17 @@
  #!/usr/bin/python3
 
- import urllib.request
+"""This script fetches https://intranet.hbtn.io/status
+    using the urllib package"""
+import urllib.request
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-    # Set the URL to fetch to a variable
-    url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    details = response.read()
 
-    # Use the with statement to open the URL and make an HTTP request
-    # The response object is automatically closed at the end of the with statement
-    with urllib.request.urlopen(url) as response:
-        # Read the contents of the response and assign to a variable
-        html = response.read()
-
-    # Display some information about the response received from the server
-    print("Body response:")
-    # Display the type of the "html" variable
-    print("\t- type: {}".format(type(html)))
-    # Display the content of the response as a sequence of bytes
-    print("\t- content: {}".format(html))
-    # Display the UTF-8 encoded content of the response as a string
-    print("\t- utf8 content: {}".format(html.decode('utf-8'))))
+print("Body response:")
+print("\t- type: {}".format(type(details)))
+print("\t- content: {}".format(details))
+print("\t- utf8 content: {}".format(details.decode('utf-8')))
 
 if __name__ == "__main__":
