beaafbac2f0f02dc197e22a39243a684aea1e843
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 04:17:04 -0700

    little Adjustment

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,24 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 
-import urllib.request
+ import urllib.request
 
-# Set the URL to fetch to a variable
-url = 'https://alx-intranet.hbtn.io/status'
 
-# Use the with statement to open the URL and make an HTTP request
-# The response object is automatically closed at the end of the with statement
-with urllib.request.urlopen(url) as response:
-    # Read the contents of the response and assign to a variable
-    html = response.read()
+    # Set the URL to fetch to a variable
+    url = 'https://alx-intranet.hbtn.io/status'
 
-# Display some information about the response received from the server
-print("Body response:")
-# Display the type of the "html" variable
-print("\t- type: {}".format(type(html)))
-# Display the content of the response as a sequence of bytes
-print("\t- content: {}".format(html))
-# Display the UTF-8 encoded content of the response as a string
-print("\t- utf8 content: {}".format(html.decode('utf-8'))))
+    # Use the with statement to open the URL and make an HTTP request
+    # The response object is automatically closed at the end of the with statement
+    with urllib.request.urlopen(url) as response:
+        # Read the contents of the response and assign to a variable
+        html = response.read()
+
+    # Display some information about the response received from the server
+    print("Body response:")
+    # Display the type of the "html" variable
+    print("\t- type: {}".format(type(html)))
+    # Display the content of the response as a sequence of bytes
+    print("\t- content: {}".format(html))
+    # Display the UTF-8 encoded content of the response as a string
+    print("\t- utf8 content: {}".format(html.decode('utf-8'))))
+
+if __name__ == "__main__":
