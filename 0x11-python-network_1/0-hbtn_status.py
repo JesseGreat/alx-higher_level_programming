8734de73a21908a0cb8d9870e373f0eba7a1ee18
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 05:08:21 -0700

    still correcting

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,16 +2,17 @@
 
 """This script fetches https://intranet.hbtn.io/status
     using the urllib package"""
+
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    details = response.read()
-
-print("Body response:")
-print("\t- type: {}".format(type(details)))
-print("\t- content: {}".format(details))
-print("\t- utf8 content: {}".format(details.decode('utf-8')))
-
 if __name__ == "__main__":
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        details = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(details)))
+        print("\t- content: {}".format(details))
+        print("\t- utf8 content: {}".format(details.decode('utf-8')))
