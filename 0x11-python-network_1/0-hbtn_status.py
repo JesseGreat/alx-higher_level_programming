83fc1f395e08915235cac146e677b9347d7c3c28
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-04-03 06:08:36 -0700

    Response Header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,13 +7,14 @@ using the urllib package.
 
 import urllib.request
 
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         details = response.read()
 
-        print("Body response:")
-        print(f"\t- type: {type(details)}")
-        print(f"\t- content: {details}")
-        print(f"\t- utf8 content: {details.decode('utf-8')}")
+    print("Body response:")
+    print(f"\t- type: {type(details)}")
+    print(f"\t- content: {details}")
+    print(f"\t- utf8 content: {details.decode('utf-8')}")
 
